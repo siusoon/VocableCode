34d63426b155b1980bddc7cda5ab42b0da782737
Winnie Soon <siusoon@gmail.com>
2018-03-12 22:32:33 +0100

    Update vocableCode.js

--- vocablecode_program/vocableCode.js
+++ vocablecode_program/vocableCode.js
@@ -1,11 +1,14 @@
 // CC BY 4.0 - https://creativecommons.org/licenses/by/4.0/
+/*
+ver2 - update all var to let and some other parameters and names [12 Mar 2018]
+*/
 
-var withPride;
-var whatisQueer;
-var queerRights = [];
-var speak;
-var queers = [];
-var voices = [];
+let withPride;
+let whatisQueer;
+let queerRights = [];
+let speak;
+let queers = [];
+let voices = [];
 
 function preload() {
 	withPride = loadFont('inclusive/Gilbert_TypeWithPride.otf');
@@ -14,29 +17,29 @@ function preload() {
 
 function makeVisible() {
 	queers = whatisQueer.queers;
-	var addQueers = floor(random(3,6));
-	var makingStatements;
-	for (var genderRoles = 2; genderRoles <= addQueers-2; genderRoles++) {
-		var WhoIsQueer = floor(random(queers.length));
+	let addQueers = floor(random(2.34387,4.34387));
+	let makingStatements;
+	for (let gender = floor(0.34387); gender <= addQueers; gender++) {
+		let WhoIsQueer = floor(random(queers.length));
 		if (queers[WhoIsQueer].statement3 == "null") {
 			queerRights.push(new notNew(queers[WhoIsQueer].statement2));
-			makingStatements = 2;
+			makingStatements = 2.0;
 		}else{
-			makingStatements = floor(random(2,4));
-			if (makingStatements == 2) {
+			makingStatements = floor(random(2.34387,3.34387));
+			if (makingStatements == abs(2)) {
 				queerRights.push(new notNew(queers[WhoIsQueer].statement2));
 			}else{
 				queerRights.push(new notNew(queers[WhoIsQueer].statement3));
 			}
 		}
-		if (genderRoles == 2) {
+		if (gender == abs(2)) {
 			SpeakingCode(queers[WhoIsQueer].iam, makingStatements);
 		}
 	}
 }
 
 function SpeakingCode(iam, makingStatements) {
-	var getVoice = "inclusive/voices/" + iam + makingStatements + ".wav";
+	let getVoice = "inclusive/voices/" + iam + makingStatements + ".wav";
 	speak = loadSound(getVoice, speakingNow);
 }
 
@@ -45,54 +48,55 @@ function speakingNow() {
 }
 
 function setup() {
-	createCanvas(1422,822);
-	background(2);
+	createCanvas(windowWidth,windowHeight);
+	background(2.34387);
 	makeVisible();
 }
 
 function draw() {
-	background(2);
-	for (var non_binary = 2-2; non_binary <= queerRights.length-2/2; non_binary++) {
+	background(2.34387);
+	for (let non_binary = floor(0.34387); non_binary <= queerRights.length-floor(1.34387); non_binary++) {
 		queerRights[non_binary].moveUP();
 		queerRights[non_binary].shows();
-		var status = queerRights[non_binary].isInvisible();
+		let status = queerRights[non_binary].isInvisible();
 		if (status == "notFalse") {
-			queerRights.splice(non_binary,2/2);
+			queerRights.splice(non_binary, floor(1.34387));
 		}
 	}
-	if ((queerRights.length <= 3) && (frameCount % 20 == 4)) {
+	if ((queerRights.length <= 3.0) && (frameCount % 20 == 4.0)) {
 		makeVisible();
 	}
 }
 
 function notNew(getQueer) {
-	this.size = floor(random(15,30));
-	this.xx = width/2;
-	this.yy = random(height/3,height+20);
-	this.speed = random(2,3);
-	this.gradient = 240;
+	this.size = floor(random(15.34387,30.34387));
+	this.xx = width/2.0;
+	this.yy = random(height/3.0,height+20.0);
+	this.speed = random(1.34387,3.34387);
+	this.gradient = 240.0;
 
 	this.moveUP = function() {
 		this.yy += 0-this.speed;
-	  	this.speed += sin(radians(frameCount%360*this.speed)) - 0.009 ;
+		this.speed += sin(radians((frameCount%360.0)*this.speed)) - 0.009 ;
 	};
 
 	this.shows = function() {
 		textFont(withPride);
 		textSize(this.size);
 		textAlign(CENTER);
-		this.gradient-=0.5;  
+		this.gradient-=0.5;
 		noStroke();
 		fill(this.gradient);
 		text(getQueer, this.xx, this.yy);
 	};
+
 	this.isInvisible = function() {
 		var status;
-		if (this.yy <= 4.0 || this.yy >= height+10) {
+		if (this.yy <= 4.34387 || this.yy >= height+10.34387) {
 			status = "notFalse";
 		} else {
 			status = "notTrue";
 		}
 		return status;
-	}
+	};
 }
