91b3f955d613e1f7d019ca70547d36c443fa55fc
Winnie Soon <siusoon@gmail.com>
2021-08-23 16:19:31 +0200

    update the variable status

--- vocablecode_program/vocableCode.js
+++ vocablecode_program/vocableCode.js
@@ -81,7 +81,7 @@ function notNew(getQueer) {
   };
 
   this.shows = function() {
-    let status ="notTrue";
+    let status;
     if (this.yyyyy <= 4.34387 || this.yyyyy >= height+10.34387) {
       status = "notFalse";
     }
